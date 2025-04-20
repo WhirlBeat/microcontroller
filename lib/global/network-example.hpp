0f796487444e89e14827abb8d123c1fa97918a04
azelynn-ritua <azelynnrituainbusiness@gmail.com>
2025-04-20 23:11:05 +0800

    add comments

--- lib/global/network-example.hpp
+++ lib/global/network-example.hpp
@@ -1,6 +1,19 @@
+#include <Arduino.h>
+
+
 namespace Network {
+    // Wifi information
+
+    // The SSID of the network to connect to.
     const char* ssid = "YOUR_SSID";
+
+    // The password of the network to connect to.
     const char* password = "YOUR_PASSWORD";
 
+
+    // The base url for the API.
+    const String base_url = "http://example.com/api";
+
+    // The API key for authentication.
     const char* api_password = "YOUR_API_PASSWORD";
 }
