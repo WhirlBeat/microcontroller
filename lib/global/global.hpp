163a503297ab80e404c42de80406d4e03c05432f
azelynn-ritua <azelynnrituainbusiness@gmail.com>
2025-04-21 22:48:33 +0800

    change pins

--- lib/global/global.hpp
+++ lib/global/global.hpp
@@ -15,9 +15,9 @@ namespace ComponentParams {
 
 
 namespace Pins {
-    const int PIN_BUTTON_LEFT = 25;
+    const int PIN_BUTTON_LEFT = 27;
     const int PIN_BUTTON_ACTION = 26;
-    const int PIN_BUTTON_RIGHT = 27;
+    const int PIN_BUTTON_RIGHT = 25;
 
     const int PIN_LEDSTRIP_DATA = 23;
 }
