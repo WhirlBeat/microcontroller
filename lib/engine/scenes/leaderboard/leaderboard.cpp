5fd07d5691ec97d46df1f69ceb47958baaac9529
azelynn-ritua <azelynnrituainbusiness@gmail.com>
2025-04-22 21:26:21 +0800

    implement leaderboards

---
+++ lib/engine/scenes/leaderboard/leaderboard.cpp
@@ -0,0 +1,139 @@
+#include "leaderboard.hpp"
+
+
+
+namespace Engine {
+    LeaderboardEntry::LeaderboardEntry(String username, int score, int placement) :
+        username(username),
+        score(score),
+        placement(placement)
+    {}
+    LeaderboardEntry::LeaderboardEntry() :
+        username(""),
+        score(0),
+        placement(0)
+    {}
+
+    LeaderboardScene::LeaderboardScene(const char* table_name, int load_count, int center_on) :
+        Scene::Scene(),
+        table_name(table_name),
+        load_count(load_count),
+        center_on(center_on)
+    {}
+
+    Scene* LeaderboardScene::tick() {
+        if (!this->is_entries_loaded) {
+            this->load_entries();
+        }
+
+        if (this->state != CONFIRM_EXIT) this->is_confirm_exit_rendered = false;
+
+        if (this->state == DISPLAY_ENTRIES) {
+            this->update_display_entries_controls();
+            if (this->current_center_idx == this->previous_center_idx) {
+                return nullptr;
+            }
+            this->previous_center_idx = this->current_center_idx;
+            this->render_entries(this->current_center_idx);
+            return nullptr;
+        }
+        if (this->state == CONFIRM_EXIT) {
+            this->tick_confirm_exit_scene_part();
+            return nullptr;
+        }
+        if (this->state == EXIT) {
+            Drivers::display_driver.clear_all();
+            return &main_menu_scene;
+        }
+
+        return nullptr;
+    }
+
+    void LeaderboardScene::load_entries() {
+        JsonDocument result = Drivers::api_driver.get_table(this->table_name, this->load_count, this->center_on);
+        JsonArray more_info = result["moreInfo"].as<JsonArray>();
+
+        this->actual_entry_count = more_info.size();
+        for (int idx = 0; idx < this->actual_entry_count; idx++) {
+            JsonObject obj = more_info[idx].as<JsonObject>();
+
+            this->entries[idx] = LeaderboardEntry(
+                String(obj["username"].as<const char*>()),
+                obj["score"].as<int>(),
+                obj["placement"].as<int>()
+            );
+        }
+
+        this->is_entries_loaded = true;
+    }
+
+    void LeaderboardScene::update_display_entries_controls() {
+        if (Drivers::button_driver_left.is_click_repeated()) {
+            this->current_center_idx--;
+            if (this->current_center_idx < 0) this->current_center_idx = this->actual_entry_count - 1;
+        }
+
+        if (Drivers::button_driver_right.is_click_repeated()) {
+            this->current_center_idx++;
+            if (this->current_center_idx >= this->actual_entry_count) this->current_center_idx = 0;
+        }
+
+        if (Drivers::button_driver_action.is_clicked()) {
+            this->state = CONFIRM_EXIT;
+        }
+    }
+
+    void LeaderboardScene::render_entries(int center_idx) {
+        Drivers::display_driver.clear_all();
+
+        int start_idx = center_idx - floor((float)(Drivers::display_driver.size_y - 1) / 2.0F);
+        if (start_idx < 0) start_idx = 0;
+
+        int end_idx = start_idx + (Drivers::display_driver.size_y - 1);
+        if (end_idx >= this->actual_entry_count) {
+            end_idx = this->actual_entry_count - 1;
+            start_idx = end_idx - (Drivers::display_driver.size_y - 1);
+        }
+
+        Serial.println(String("a ") + start_idx + " b " + end_idx + " c " + center_idx);
+
+        for (int idx = start_idx; idx <= end_idx; idx++) {
+            LeaderboardEntry entry = this->entries[idx];
+
+            int current_row = idx - start_idx;
+            Serial.println(current_row);
+            if (idx == center_idx) {
+                Drivers::display_driver.print_at(1, current_row, ">");
+            }
+
+            Drivers::display_driver.print_at(
+                2, current_row,
+                (String("#") + entry.placement + " " + entry.username + " " + entry.score).c_str()
+            );
+        }
+    }
+
+
+
+    void LeaderboardScene::tick_confirm_exit_scene_part() {
+        if (!this->is_confirm_exit_rendered) {
+            Drivers::display_driver.clear_all();
+            Drivers::display_driver.print_center(0, "Exit leaderboard?");
+
+            this->confirm_exit_scene_part.force_render();
+            this->is_confirm_exit_rendered = true;
+        }
+
+        this->confirm_exit_scene_part.tick();
+
+        if (Drivers::button_driver_action.is_clicked()) {
+            if (this->confirm_exit_scene_part.get_selected_idx() == 1) {
+                this->state = EXIT;
+            }
+            else {
+                this->previous_center_idx = 1000;
+                this->state = DISPLAY_ENTRIES;
+            }
+        }
+    }
+}
---
+++ lib/engine/scenes/leaderboard/leaderboard.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <drivers.hpp>
+
+#include "../../scene_parts/scene_parts.hpp"
+#include "../../base/base.hpp"
+#include "../main_menu/main_menu.hpp"
+
+
+namespace Engine {
+    class LeaderboardEntry {
+    public:
+        String username;
+        int score;
+        int placement;
+
+        LeaderboardEntry(String username, int score, int placement);
+        LeaderboardEntry();
+    };
+
+
+
+    class LeaderboardScene : public Scene {
+    public:
+        enum State {
+            DISPLAY_ENTRIES,
+            CONFIRM_EXIT,
+            EXIT
+        };
+
+
+        State state = DISPLAY_ENTRIES;
+
+
+        static const size_t choices_count = 2;
+        const char* choices[choices_count] = {
+            "No", "Yes"
+        };
+
+        SelectMenuScenePart confirm_exit_scene_part{choices, choices_count, 2};
+        bool is_confirm_exit_rendered = false;
+
+
+        const char* table_name;
+        int load_count;
+        int center_on;
+
+        static const int MAX_ENTRY_COUNT = 20;
+        LeaderboardEntry entries[MAX_ENTRY_COUNT];
+        int actual_entry_count = 0;
+        bool is_entries_loaded = false;
+
+        int current_center_idx = 0;
+        int previous_center_idx = 1000;
+
+        LeaderboardScene(const char* table_name, int load_count, int center_on = -1);
+
+        Scene* tick() override;
+
+        void load_entries();
+        void update_display_entries_controls();
+        void render_entries(int center_idx = 0);
+
+        void tick_confirm_exit_scene_part();
+    };
+}
