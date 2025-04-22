bd847a688c8112777420b7077f1ec712a50fa6c1
azelynn-ritua <azelynnrituainbusiness@gmail.com>
2025-04-22 21:34:36 +0800

    test leaderboard scene

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,9 @@
 
 
 
-Engine::TimingModePlayScene scene{};
+Engine::LeaderboardScene scene{Drivers::timing_table_name, 10, 3};
 
-Engine::SceneLoader scene_loader{scene};
+Engine::SceneLoader scene_loader{&scene};
 
 Tools::TPS tps;
 
@@ -20,11 +20,48 @@ void setup() {
 
     Serial.begin(115200);
     while (!Serial) delay(100);
+
+    Drivers::api_driver.begin();
 }
 
 void loop() {
     scene_loader.tick();
-    tps.tick();
+    // tps.tick();
 
     delay(1);
 }
+
+
+// #include <WiFi.h>
+// #include <HTTPClient.h>
+
+
+// void setup() {
+//     Serial.begin(115200); 
+
+//     Drivers::network_driver.begin();
+// }
+
+// void loop() {
+//     if (WiFi.status() != WL_CONNECTED) {
+//         Serial.println("WiFi not connected, reconnecting...");
+//         WiFi.disconnect();
+//         WiFi.begin(NetworkParams::ssid, NetworkParams::password);
+//         while(WiFi.status() != WL_CONNECTED) {
+//             delay(500);
+//             Serial.print(".");
+//         }
+//     }
+
+//     HTTPClient http;
+//     http.begin((NetworkParams::base_url + "/hello").c_str());
+
+//     int httpResponseCode = http.GET();
+//     String payload = http.getString();
+//     Serial.println(payload);
+
+//     http.end();
+
+//     Serial.println("Connected.");
+//     delay(5000);
+// }
