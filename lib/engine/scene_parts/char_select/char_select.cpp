8d50c3cc88ca350852ab8c80811dba46a7fae91d
azelynn-ritua <azelynnrituainbusiness@gmail.com>
2025-04-23 17:02:16 +0800

    implement char select

---
+++ lib/engine/scene_parts/char_select/char_select.cpp
@@ -0,0 +1,38 @@
+#include <colors.hpp>
+
+#include "char_select.hpp"
+
+
+
+namespace Engine {
+    CharSelectScenePart::CharSelectScenePart(int selected_idx, int x, int y) :
+        selected_idx(selected_idx),
+        x(x), y(y)
+    {
+        if (selected_idx >= CHAR_SELECT_CHOICES_SIZE) {
+            selected_idx = 0;
+        }
+    }
+
+
+    void CharSelectScenePart::tick() {
+        if (Drivers::button_driver_left.is_clicked()) {
+            selected_idx--;
+            if (selected_idx < 0) {
+                selected_idx = CHAR_SELECT_CHOICES_SIZE - 1;
+            }
+        }
+
+        if (Drivers::button_driver_right.is_clicked()) {
+            selected_idx++;
+            if (selected_idx >= CHAR_SELECT_CHOICES_SIZE) {
+                selected_idx = 0;
+            }
+        }
+
+        char c_str[2];
+        c_str[0] = CHAR_SELECT_CHOICES[selected_idx];
+        c_str[1] = '\0';
+        Drivers::display_driver.print_at(x, y, c_str);
+    }
+}
---
+++ lib/engine/scene_parts/char_select/char_select.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <drivers.hpp>
+#include "../../base/base.hpp"
+#include "../arrow/arrow.hpp"
+
+
+namespace Engine {
+    const size_t CHAR_SELECT_CHOICES_SIZE = 36;
+    const char CHAR_SELECT_CHOICES[CHAR_SELECT_CHOICES_SIZE] = {
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+        'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+        'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3',
+        '4', '5', '6', '7', '8', '9'
+    };
+
+    class CharSelectScenePart : public ScenePart {
+    public:
+        int selected_idx;
+        int x;
+        int y;
+
+        CharSelectScenePart(int selected_idx = 0, int x = 0, int y = 0);
+
+        void tick() override;
+    };
+}
