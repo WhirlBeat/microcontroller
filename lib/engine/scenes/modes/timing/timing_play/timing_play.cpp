a363d8dadf6391bb7368e56e16ceaa4507b8cbc3
azelynn-ritua <azelynnrituainbusiness@gmail.com>
2025-04-25 10:59:52 +0800

    connect play to score submit

--- lib/engine/scenes/modes/timing/timing_play/timing_play.cpp
+++ lib/engine/scenes/modes/timing/timing_play/timing_play.cpp
@@ -27,22 +27,53 @@ namespace Engine {
 
     void TimingModePlayScene::tick() {
         this->timing_engine.tick();
+
+        Drivers::display_driver.clear_row(this->ROW_ATTEMPTS);
+
+        String attempts_str = String("Attempt ") + (this->attempt_idx + 1) + "/" + this->attempts;
+        Drivers::display_driver.print_center(this->ROW_ATTEMPTS, attempts_str.c_str());
+
+
         if (this->timing_engine.is_stop_clicked) {
             int score = this->timing_engine.calculate_score_weighted();
 
-            this->total_score += score;
+            float multiplier = get_timing_mod_total_multiplier(this->selected_mods, this->selected_mods_count);
+            this->total_score += floor((float)score * multiplier);
 
             Drivers::display_driver.clear_all();
 
-            String score_str = String(score);
-            Drivers::display_driver.print_center(0, score_str.c_str());
+            if (score == this->timing_engine.max_score) {
+                this->perfects++;
+            }
 
-            String total_score_str = String(this->total_score);
-            Drivers::display_driver.print_center(1, total_score_str.c_str());
+            if (this->perfects != 0) {
+                String perfects_str = String("PERFECTS: ") + this->perfects;
+                Drivers::display_driver.print_center(this->ROW_PERFECTS, perfects_str.c_str());
+            }
+
+            String score_str =
+                (
+                    score == this->timing_engine.max_score
+                        ? String("PERFECT +") + score
+                        : String ("Prev: +") + score
+                ) +
+                " x" + String(multiplier, 2);
+            Drivers::display_driver.print_center(this->ROW_SCORE_SINGLE, score_str.c_str());
+
+            String total_score_str = String("TOTAL: ") + this->total_score;
+            Drivers::display_driver.print_center(this->ROW_SCORE_TOTAL, total_score_str.c_str());
         }
 
         if (this->timing_engine.is_finished) {
             this->restart_engine();
+            this->attempt_idx++;
+
+            if (this->attempt_idx >= this->attempts) {
+                this->score_submit.init(Drivers::timing_table_name, this->total_score);
+                this->score_submit.begin();
+
+                scene_loader.switch_scene(&this->score_submit);
+            }
         }
     }
 }
--- lib/engine/scenes/modes/timing/timing_play/timing_play.hpp
+++ lib/engine/scenes/modes/timing/timing_play/timing_play.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <base/base.hpp>
+#include "../../../score_submit/score_submit.hpp"
 #include "../timing_engine/timing_engine.hpp"
 #include "../timing_mods/timing_mods.hpp"
 
@@ -14,6 +15,18 @@ namespace Engine {
 
         TimingEngineScenePart timing_engine{};
         int total_score = 0;
+        int perfects = 0;
+
+        int attempts = 5;
+        int attempt_idx = 0;
+
+        ScoreSubmitScene score_submit{};
+
+
+        static const int ROW_ATTEMPTS = 0;
+        static const int ROW_PERFECTS = 1;
+        static const int ROW_SCORE_SINGLE = 2;
+        static const int ROW_SCORE_TOTAL = 3;
 
         TimingModePlayScene();
 
